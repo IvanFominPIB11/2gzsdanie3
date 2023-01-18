b049ee8c4109691d90d06a3384e2d01c50c2f86c
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-18 14:51:58 +0700

    Добавьте файлы проекта.

---
+++ 2gzsdanie3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _2gzsdanie3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number, delenie;
+            number = Int32.Parse(
+                Interaction.InputBox(
+                    "Введите целое число:")
+                );
+            delenie = number / 4;
+            string txt = "Число " + number + "";
+            bool nemenshe = delenie < 10;
+            txt += (nemenshe == false ? " число деленная на 4 и больше 10" : " число меньше 10");
+            MessageBox.Show(txt);
+        }
+    }
+}
